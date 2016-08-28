87f16b207590170ca9fc72247c68ecd8447585e3
vic6 <vicmatthews116@gmail.com>
2016-08-28 10:02:57 -0700

    Add allergies check and final message

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,51 @@
-prob = "Results inconclusive"
 
-puts "What is your name?"
-name = gets.chomp.downcase!
+puts "Enter number of employees to be processed."
+total = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+total.times do 
+  prob = "Results inconclusive"
 
-puts "What year were you born?"
-year = gets.chomp.to_i
+  puts "What is your name?"
+  name = gets.chomp.downcase!
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-food = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+  puts "What year were you born?"
+  year = gets.chomp.to_i
 
-if 2016 - year == age and (food == 'yes' or insurance == 'yes')
-  prob = "Probably not a vampire."
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  food = gets.chomp
+
+  puts "Would you like to enroll in the company's health insurance?"
+  insurance = gets.chomp
+
+  if 2016 - year == age and (food == 'yes' or insurance == 'yes')
+    prob = "Probably not a vampire."
+  end
+
+  if 2016 - year != age.to_i and (food == "no" or insurance == 'no')
+    prob = "Probably a vampire."
+  end
+
+  if 2016 - year != age.to_i and (food == "no" and insurance == 'no')
+    prob = "Almost certainly a vampire."
+  end
+
+  if name == "drake cula" or name == "tu fang"
+    prob = "Definitely a vampire."
+  end
+  
+  allergies = nil
+  while allergies != "done"
+    puts "Enter your allergies one at at time, when finished enter 'done'."
+    allergies = gets.chomp
+    if allergies == 'sunshine'
+      prob = "Probably a vampire."
+      break
+    end
+  end
+  puts prob
+
 end
-
-if 2016 - year != age.to_i and (food == "no" or insurance == 'no')
-  prob = "Probably a vampire."
-end
-
-if 2016 - year != age.to_i and (food == "no" and insurance == 'no')
-  prob = "Almost certainly a vampire."
-end
-
-if name == "drake cula" or name == "tu fang"
-  prob = "Definitely a vampire."
-end
-
-p prob
+p "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
