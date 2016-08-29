09cc6c101ba7c81c2d975e0e5bcf5cdfeece2c46
vic6 <vicmatthews116@gmail.com>
2016-08-28 23:21:45 -0700

    Fix control flow

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,15 @@
 
 puts "Enter number of employees to be processed."
 total = gets.chomp.to_i
-
-total.times do 
-  prob = "Results inconclusive"
-
+count = 1
+#for each employee, loop will run
+while count <= total 
+  #resets prob on each iteration
+  prob = "Results Inconclusive."
+  puts "Employee #{count}"
   puts "What is your name?"
-  name = gets.chomp.downcase!
-
+  name = gets.chomp.downcase
+  
   puts "How old are you?"
   age = gets.chomp.to_i
 
@@ -19,33 +21,38 @@ total.times do
 
   puts "Would you like to enroll in the company's health insurance?"
   insurance = gets.chomp
-
+  
+  #age adds up and says yes to garlic and insurance
   if 2016 - year == age and (food == 'yes' or insurance == 'yes')
     prob = "Probably not a vampire."
   end
-
-  if 2016 - year != age.to_i and (food == "no" or insurance == 'no')
-    prob = "Probably a vampire."
-  end
-
+  #age doesn't add up and says no to garlic and insurance
   if 2016 - year != age.to_i and (food == "no" and insurance == 'no')
     prob = "Almost certainly a vampire."
   end
-
+  #age doesn't add up and says no garlic or insurance
+  if 2016 - year != age.to_i and (food == "no" or insurance == 'no')
+    prob = "Probably a vampire."
+  end
   if name == "drake cula" or name == "tu fang"
     prob = "Definitely a vampire."
   end
-  
+
+  #checks for allergies
   allergies = nil
   while allergies != "done"
     puts "Enter your allergies one at at time, when finished enter 'done'."
     allergies = gets.chomp
+    #exits loop if allergen is sunshine
     if allergies == 'sunshine'
       prob = "Probably a vampire."
       break
     end
   end
+  count += 1
   puts prob
-
+  #add new line for readability
+  puts
 end
+
 p "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
