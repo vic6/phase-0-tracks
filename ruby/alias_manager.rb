f89b7ca3d95c880fba968c16c682ab6dca4528e5
vic6 <vicmatthews116@gmail.com>
2016-09-02 16:31:12 -0700

    First attempt at solving solo challenge

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,1 +1,42 @@
+#FIRST ATTEMPT
 #prompt for real name
+# swap first and last name
+# spilt charactors, if a vowel, change to next vowel, if consonants, switch to next consonant
+
+
+def nameSwitch(name)
+  vowels = 'aeiou'
+  new_name = ''
+  newname = name.split(' ').reverse.join(' ')
+end
+
+def charactor(str)
+  str = nameSwitch(str)
+  vowels = 'aeiouAEIOU'
+  consonants = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+  newStr = ''
+  str = str.chars
+  for letter in str 
+    if vowels.include? letter
+      if letter == 'u'
+        letter = 'a'
+        newStr << letter
+      else letter = vowels[vowels.index(letter) + 1]
+        newStr << letter
+      end
+    elsif consonants.include? letter
+      if letter == 'z'
+        letter = 'b'
+        newStr << letter
+      else letter = consonants[consonants.index(letter) + 1]
+        newStr << letter
+      end
+    else
+      newStr << letter
+    end
+  end
+  newStr
+end
+
+p charactor('Felicia Torres')
+
