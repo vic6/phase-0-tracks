e0ed309316fd018692ba3deeed6b27005e27e8c9
vic6 <vicmatthews116@gmail.com>
2016-09-17 17:00:39 -0700

    Complete release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,10 @@ colors.push('yellow');
 names.push('Lee');
 console.log(colors);
 console.log(names);
+
+var horses = {};
+for(var i = 0; i < names.length; i++) {
+  horses[names[i]] = colors[i];
+}
+
+console.log(horses);
