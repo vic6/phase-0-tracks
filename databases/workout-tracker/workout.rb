88f6ef193db68eb8879f08656db7d4022006c2c3
vic6 <vicmatthews116@gmail.com>
2016-09-26 09:07:48 -0700

    Add display method

--- databases/workout-tracker/workout.rb
+++ databases/workout-tracker/workout.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'date'
 
 #create db
 $db = SQLite3::Database.new('workout.db')
@@ -34,7 +35,7 @@ $db.execute("INSERT INTO Exercise (name) VALUES ('lockdog press')")
 #exercises = db.execute("SELECT name FROM Exercise")
 
 
-ex = [[1, "lockdog press"]]
+#ex = [[1, "lockdog press"]]
 # ex.each do |exer|
 #   if name == exer[1]
 #     print 'already there'
@@ -50,21 +51,52 @@ def add_exercise(name)
 end
 
 
-
-add_exercise('carndogs')
-add_exercise('pie-ups')
-add_exercise('chin-ups')
-add_exercise('boss pops')
-add_exercise('carndogs')
-
 #
 def add_intensity(exercise, date, sets, reps, weight)
   exercise_id = $db.execute("SELECT Exercise.id FROM Exercise WHERE Exercise.name = '#{exercise}'")
-  $db.execute("INSERT INTO Workouts (date, sets, reps, weight, exercise_id) VALUES (?, ?, ?, ?, ?)", [date, sets, reps, weight, exercise_id])
+  if not exercise_id.empty? 
+    $db.execute("INSERT INTO Workouts (date, sets, reps, weight, exercise_id) VALUES (?, ?, ?, ?, ?)", [date, sets, reps, weight, exercise_id])
+  end
+  if exercise_id.empty?
+    print "Exercise not found."
+  end
 end
 
-add_intensity('lockdog press', '2/12/1989', 4, 10, 50)
+# add_intensity('carndogs', '02/12/1989', 5, 10, 250)
+# add_intensity('boss popsdf', '02/12/1989', 5, 10, 250)
+
+
+def display
+  print_out = $db.execute("SELECT Exercise.name, Workouts.date, Workouts.weight FROM Workouts JOIN Exercise ON Exercise.id = Workouts.exercise_id ")
+  #print print_out
+  puts "Date: #{print_out[0][1]}"
+  puts "Exercise: #{print_out[0][0]}"
+
+end
+
 # puts exercise_id
 
-#puts $db.execute("SELECT Exercise.id FROM Exercise WHERE Exercise.name = 'pie-ups'")
-#
+
+#prompt user if they would like to add exercise or update existing workout
+
+input = nil
+until input == 'exit'
+  puts "Would you like to add(add) an exercise?(Type 'exit' to quit.)"
+  input = gets.chomp
+  if input == 'add'
+    puts "Enter the name of the exercise."
+    name = gets.chomp
+    add_exercise(name)
+    day = DateTime.now
+    date = day.strftime "%m/%d/%Y"
+    puts "Enter the sets, reps and weight in lbs in this format(ex: 4 sets of 12 with 200)"
+    intensity = gets.chomp.split(' ')
+    sets = intensity[0]
+    reps = intensity[3]
+    weight = intensity[-1]
+    add_intensity(name, date, sets, reps, weight)
+  end
+end
+
+
+display
